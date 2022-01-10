6012f0ab4502118fb99fbaf36c6b76098101515b
Ahmed Arafa <you@example.com>
2022-01-10 10:18:43 +0200

    fixing styles in IOS issue

--- pages/CourseDetails.tsx
+++ pages/CourseDetails.tsx
@@ -21,6 +21,7 @@ import CommentsSection from "modules/Course details/Comments section/CommentsSec
 import MobileNavTabsBar from "modules/Course details/Mobile nav tabs bar/MobileNavTabsBar";
 import MobileCheckoutBar from "modules/Course details/Mobile checkout bar/MobileCheckoutBar";
 import MonthlySubscriptionCard from "modules/Course details/Monthly subscription card/MonthlySubscriptionCard";
+import Head from "next/head";
 
 import { Row, Col } from "react-bootstrap";
 
@@ -138,6 +139,12 @@ export default function CourseDetails() {
   }, []);
   return (
     <>
+    <Head>
+        <title>كورسات دورات اون لاين عن بعد مجانية و معتمدة | تدرب</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="نقدم لك عبر موقع تدرب افضل الدورات المعتمدة عن بعد مع خبراء مختصين في مختلف المجالات. سجل في الدورات التدريبية المجانية لدينا اون لاين لتطوير مهاراتك" />
+        <link rel="icon" href="/images/logo.svg" />
+      </Head>
       <Navbar />
       <MobileNavTabsBar />
       <MobileCheckoutBar />
--- pages/SignUp.tsx
+++ pages/SignUp.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
 import Navbar from "common/components/Navbar/Navbar";
 import SignupPage from "modules/Sign Up/SignUp page/SignupPage";
+import Head from "next/head";
 
 export default function SignUp() {
     return (
         <>
+        <Head>
+        <title>كورسات دورات اون لاين عن بعد مجانية و معتمدة | تدرب</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="نقدم لك عبر موقع تدرب افضل الدورات المعتمدة عن بعد مع خبراء مختصين في مختلف المجالات. سجل في الدورات التدريبية المجانية لدينا اون لاين لتطوير مهاراتك" />
+        <link rel="icon" href="/images/logo.svg" />
+      </Head>
         <Navbar/>
         <SignupPage/>
         </>
