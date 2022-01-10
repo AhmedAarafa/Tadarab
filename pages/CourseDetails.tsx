c1e4bda83af1c8d92153f457c7c953886f0d010a
Ahmed Arafa <you@example.com>
2022-01-10 10:43:06 +0200

    fixing styles in IOS issue

--- pages/CourseDetails.tsx
+++ pages/CourseDetails.tsx
@@ -141,7 +141,7 @@ export default function CourseDetails() {
     <>
     <Head>
         <title>كورسات دورات اون لاين عن بعد مجانية و معتمدة | تدرب</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale = 1.0" />
         <meta name="description" content="نقدم لك عبر موقع تدرب افضل الدورات المعتمدة عن بعد مع خبراء مختصين في مختلف المجالات. سجل في الدورات التدريبية المجانية لدينا اون لاين لتطوير مهاراتك" />
         <link rel="icon" href="/images/logo.svg" />
       </Head>
--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home: NextPage = () => {
     <>
       <Head>
         <title>كورسات دورات اون لاين عن بعد مجانية و معتمدة | تدرب</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0,maximum-scale = 1.0" />
         <meta name="description" content="نقدم لك عبر موقع تدرب افضل الدورات المعتمدة عن بعد مع خبراء مختصين في مختلف المجالات. سجل في الدورات التدريبية المجانية لدينا اون لاين لتطوير مهاراتك" />
         <link rel="icon" href="/images/logo.svg" />
       </Head>
