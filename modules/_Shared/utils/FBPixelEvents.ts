b3c7c30eeb6251dde18f445e166508fe64c7c43d
AhmedAarafa <96011376+AhmedAarafa@users.noreply.github.com>
2022-03-27 15:43:52 +0200

    Update FBPixelEvents.ts

--- modules/_Shared/utils/FBPixelEvents.ts
+++ modules/_Shared/utils/FBPixelEvents.ts
@@ -5,7 +5,7 @@ export const FBPixelEventsHandler = (trackingEvents:any,customData:any):any =>{
     
     let tadarabFbPixel = new TadarabFBPixel();
     trackingEvents.forEach((ev: any) => {
-      trackingEvents.setEventId(ev.event_id);
+      tadarabFbPixel.setEventId(ev.event_id);
       tadarabFbPixel.eventHandler(ev.event_name, customData);
     });
     
