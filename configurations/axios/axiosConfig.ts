4ac803f28367a047a2f61a0f5b0e48641cf3d3f3
hirentariwala <hirentariwala19@gmail.com>
2022-04-06 16:59:24 +0530

    loader issue fix

--- configurations/axios/axiosConfig.ts
+++ configurations/axios/axiosConfig.ts
@@ -1,13 +1,12 @@
-import axios from "axios";
-import { useDispatch } from "react-redux";
-import { setErrorText } from "../redux/actions/errorText";
-import {toggleLoader} from "modules/_Shared/utils/toggleLoader";
+import axios from 'axios'
+import { useDispatch } from 'react-redux'
+import { setErrorText } from '../redux/actions/errorText'
+// import { toggleLoader } from 'modules/_Shared/utils/toggleLoader'
 
 export const axiosInstance = axios.create({
   // baseURL: "https://developer.tadarab.com/wp-json/api/",
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
-});
-
+})
 
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
@@ -16,33 +15,33 @@ axiosInstance.interceptors.request.use(
     // show loader
     // config.params = config.params || {};
     // config.params['auth'] = 'token';
-    toggleLoader("show");
-    function randomString(length:any, chars:any) {
-      let result = '';
-      for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)];
-      return result;
-  }
-
-  if(localStorage.getItem("X-Session-Id")){
-    config.headers['X-Session-Id'] =  `${localStorage.getItem("X-Session-Id")}`;
-  }else{
-    let rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ');
-    config.headers['X-Session-Id'] =  rString;
-    localStorage.setItem("X-Session-Id",rString);
-  }
-
-    if(localStorage.getItem("token")){
-      config.headers.Authorization =  `Bearer ${localStorage.getItem("token")}`;
+    // toggleLoader("show");
+    function randomString(length: any, chars: any) {
+      let result = ''
+      for (var i = length; i > 0; --i) result += chars[Math.floor(Math.random() * chars.length)]
+      return result
     }
-    return config;
+
+    if (localStorage.getItem('X-Session-Id')) {
+      config.headers['X-Session-Id'] = `${localStorage.getItem('X-Session-Id')}`
+    } else {
+      let rString = randomString(32, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ')
+      config.headers['X-Session-Id'] = rString
+      localStorage.setItem('X-Session-Id', rString)
+    }
+
+    if (localStorage.getItem('token')) {
+      config.headers.Authorization = `Bearer ${localStorage.getItem('token')}`
+    }
+    return config
   },
   function (error: any) {
     // Do something with request error
-    toggleLoader("show");
+    // toggleLoader("show");
 
-    return Promise.reject(error);
+    return Promise.reject(error)
   }
-);
+)
 
 // Add a response interceptor
 axiosInstance.interceptors.response.use(
@@ -52,19 +51,17 @@ axiosInstance.interceptors.response.use(
     // hide loader
     // console.log("response", response);
     // response.data.push("test")
-    toggleLoader("hide");
+    // toggleLoader('hide')
 
-
-    return response;
+    return response
   },
   function (error: any) {
     // console.log(error.response.data);
-    toggleLoader("hide");
+    // toggleLoader('hide')
 
-  
-    return error.response;
+    return error.response
     // Any status codes that falls outside the range of 2xx cause this function to trigger
     // Do something with response error
     // return Promise.reject(error);
   }
-);
+)
--- pages/HomePage.tsx
+++ pages/HomePage.tsx
@@ -1,5 +1,5 @@
-import React,{ useState, useEffect } from 'react';
-import dynamic from 'next/dynamic';
+import React, { useState, useEffect } from 'react'
+import dynamic from 'next/dynamic'
 // import Navbar from 'common/Navbar/Navbar';
 // import HeroSection from 'modules/Home page/Hero section/HeroSection';
 // import LatestCourses from 'modules/Home page/Latest courses/LatestCourses';
@@ -15,107 +15,99 @@ import dynamic from 'next/dynamic';
 // import AboutTadarab from 'modules/Home page/About Tadarab/AboutTadarab';
 // import JoinUs from 'modules/Home page/Join us/JoinUs';
 // import Footer from 'common/Footer/Footer';
-import { Container } from "react-bootstrap";
-import withAuth from "configurations/auth guard/AuthGuard";
-import { axiosInstance } from "configurations/axios/axiosConfig";
-import { useDispatch, useSelector } from "react-redux";
-import { setHomePageData } from "configurations/redux/actions/homePageData"; 
-import TadarabFBPixel from "modules/_Shared/utils/fbPixel";
-import { GetStaticProps } from 'next';
-import { GAProductimpressionEventHandler } from "modules/_Shared/utils/GAEvents";
-import useInView from "react-cool-inview";
-import {toggleLoader} from "modules/_Shared/utils/toggleLoader";
-import { FBPixelEventsHandler } from 'modules/_Shared/utils/FBPixelEvents';
+import { Container } from 'react-bootstrap'
+import withAuth from 'configurations/auth guard/AuthGuard'
+import { axiosInstance } from 'configurations/axios/axiosConfig'
+import { useDispatch, useSelector } from 'react-redux'
+import { setHomePageData } from 'configurations/redux/actions/homePageData'
+import TadarabFBPixel from 'modules/_Shared/utils/fbPixel'
+import { GetStaticProps } from 'next'
+import { GAProductimpressionEventHandler } from 'modules/_Shared/utils/GAEvents'
+import useInView from 'react-cool-inview'
+import { toggleLoader } from 'modules/_Shared/utils/toggleLoader'
+import { FBPixelEventsHandler } from 'modules/_Shared/utils/FBPixelEvents'
 // import ReactPixel from 'react-facebook-pixel';
 
-const Navbar = dynamic(() => import("common/Navbar/Navbar"));
-const HeroSection = dynamic(() => import("modules/Home page/Hero section/HeroSection"));
-const LatestCourses = dynamic(() => import("modules/Home page/Latest courses/LatestCourses"));
-const CoursesDepartments = dynamic(() => import("modules/Home page/Courses departments/CoursesDepartments"));
-const LiveCourses = dynamic(() => import("modules/Home page/Live courses/LiveCourses"));
-const Consultation = dynamic(() => import("modules/Home page/Consultations/Consultation"));
-const HowToLearnOnTadarab = dynamic(() => import("modules/Home page/How to learn on tadarab/HowToLearnOnTadarab"));
-const Books = dynamic(() => import("modules/Home page/Books/Books"));
-const Statistics = dynamic(() => import("modules/Home page/Statistics/Statistics"));
-const WhyTadarab = dynamic(() => import("modules/Home page/Why Tadarab/WhyTadarab"));
-const LearnFromTheBest = dynamic(() => import("modules/Home page/Learn from the best/LearnFromTheBest"));
-const JoinAsATrainer = dynamic(() => import("modules/Home page/Join as a trainer/JoinAsATrainer"));
-const EducationalGuide = dynamic(() => import("modules/Home page/Educational guide/EducationalGuide"));
-const AboutTadarab = dynamic(() => import("modules/Home page/About Tadarab/AboutTadarab"));
-const JoinUs = dynamic(() => import("modules/Home page/Join us/JoinUs"));
-const Footer = dynamic(() => import("common/Footer/Footer"));
-
-
+const Navbar = dynamic(() => import('common/Navbar/Navbar'))
+const HeroSection = dynamic(() => import('modules/Home page/Hero section/HeroSection'))
+const LatestCourses = dynamic(() => import('modules/Home page/Latest courses/LatestCourses'))
+const CoursesDepartments = dynamic(() => import('modules/Home page/Courses departments/CoursesDepartments'))
+const LiveCourses = dynamic(() => import('modules/Home page/Live courses/LiveCourses'))
+const Consultation = dynamic(() => import('modules/Home page/Consultations/Consultation'))
+const HowToLearnOnTadarab = dynamic(() => import('modules/Home page/How to learn on tadarab/HowToLearnOnTadarab'))
+const Books = dynamic(() => import('modules/Home page/Books/Books'))
+const Statistics = dynamic(() => import('modules/Home page/Statistics/Statistics'))
+const WhyTadarab = dynamic(() => import('modules/Home page/Why Tadarab/WhyTadarab'))
+const LearnFromTheBest = dynamic(() => import('modules/Home page/Learn from the best/LearnFromTheBest'))
+const JoinAsATrainer = dynamic(() => import('modules/Home page/Join as a trainer/JoinAsATrainer'))
+const EducationalGuide = dynamic(() => import('modules/Home page/Educational guide/EducationalGuide'))
+const AboutTadarab = dynamic(() => import('modules/Home page/About Tadarab/AboutTadarab'))
+const JoinUs = dynamic(() => import('modules/Home page/Join us/JoinUs'))
+const Footer = dynamic(() => import('common/Footer/Footer'))
 
 function HomePage() {
-  const dispatch = useDispatch();
-  const homePageData = useSelector((state:any) => state.homePageData);
-  
-  useEffect(() => {
-      const countryCode:any = localStorage.getItem("countryCode");
-        axiosInstance
-        .get(`home/?country_code=${localStorage.getItem("countryCode")}`)
-        .then(function (response:any) {
-            dispatch(setHomePageData(response.data.data));
-            
-            FBPixelEventsHandler(response.data.fb_tracking_events,null);
+  const dispatch = useDispatch()
+  const homePageData = useSelector((state: any) => state.homePageData)
 
-        })
-        .catch(function (error) {
-          console.log(error); 
-        });
-        
-        
-    window.addEventListener("scroll" , ()=>{
-      GAProductimpressionEventHandler("latest-courses-card");
+  useEffect(() => {
+    const countryCode: any = localStorage.getItem('countryCode')
+    toggleLoader('show')
+    axiosInstance
+      .get(`home/?country_code=${localStorage.getItem('countryCode')}`)
+      .then(function (response: any) {
+        toggleLoader('hide')
+        dispatch(setHomePageData(response.data.data))
+
+        FBPixelEventsHandler(response.data.fb_tracking_events, null)
+      })
+      .catch(function (error) {
+        toggleLoader('hide')
+        console.log(error)
+      })
+
+    window.addEventListener('scroll', () => {
+      GAProductimpressionEventHandler('latest-courses-card')
     })
 
     return () => {
-      window.removeEventListener("scroll", ()=>{
-        return;
+      window.removeEventListener('scroll', () => {
+        return
       })
     }
-    
-  }, []);
-
+  }, [])
 
   const { observe, inView } = useInView({
     unobserveOnEnter: true,
-    onEnter: ({ scrollDirection, entry, observe, unobserve }) => {
-    },
-    onLeave: ({ scrollDirection, entry, observe, unobserve }) => {
-    },
-  });
+    onEnter: ({ scrollDirection, entry, observe, unobserve }) => {},
+    onLeave: ({ scrollDirection, entry, observe, unobserve }) => {},
+  })
 
-
-    return (
-        <>
-        <Container fluid="xxl" >
-            <Navbar/>
-            <HeroSection/>
-            <LatestCourses />
-            <CoursesDepartments/>
-            <LiveCourses/>
-            <HowToLearnOnTadarab/>
-            {/* <Consultation/> */}
-            {/* { <div  ref={observe} >
+  return (
+    <>
+      <Container fluid='xxl'>
+        <Navbar />
+        <HeroSection />
+        <LatestCourses />
+        <CoursesDepartments />
+        <LiveCourses />
+        <HowToLearnOnTadarab />
+        {/* <Consultation/> */}
+        {/* { <div  ref={observe} >
               { inView && <Books />}
             </div>
              } */}
-            {/* <Books /> */}
-            <Statistics/>
-            <WhyTadarab/>
-            <LearnFromTheBest/>
-            <JoinAsATrainer/>
-            <EducationalGuide/>
-            <AboutTadarab/>
-            <JoinUs/>
-            <Footer/>
-        </Container>
-            
-        </>
-    )
-};
+        {/* <Books /> */}
+        <Statistics />
+        <WhyTadarab />
+        <LearnFromTheBest />
+        <JoinAsATrainer />
+        <EducationalGuide />
+        <AboutTadarab />
+        <JoinUs />
+        <Footer />
+      </Container>
+    </>
+  )
+}
 
-
-export default HomePage;
+export default HomePage
