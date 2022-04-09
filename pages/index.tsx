83d27b74376f64e8eff8379eee3ee37f53bdc6af
AhmedAarafa <ahmed.arafa@tadarab.com>
2022-04-09 09:52:07 +0200

    :D

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
         <title>كورسات دورات اون لاين عن بعد مجانية و معتمدة | تدرب</title>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta name="description" content="نقدم لك عبر موقع تدرب افضل الدورات المعتمدة عن بعد مع خبراء مختصين في مختلف المجالات. سجل في الدورات التدريبية المجانية لدينا اون لاين لتطوير مهاراتك" />
-        <link rel="icon" href="/images/favicon.ico" />
+        <link rel="icon" href="/favicon.ico" />
         {/* <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s)
       {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
       n.callMethod.apply(n,arguments):n.queue.push(arguments)};
