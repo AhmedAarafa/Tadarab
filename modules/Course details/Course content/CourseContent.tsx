0d5747ae73a8751c724e249c471f8b99bab12951
AhmedAarafa <96011376+AhmedAarafa@users.noreply.github.com>
2022-04-11 15:19:54 +0200

    Update CourseContent.tsx

--- modules/Course details/Course content/CourseContent.tsx
+++ modules/Course details/Course content/CourseContent.tsx
@@ -43,20 +43,11 @@ export default function CourseContent() {
 
     // }
 
-<<<<<<< HEAD
-    return (
-        <>
-            <div className={styles["course-content"]}>
-                <div id="course-content" className={styles["course-content__scrollspy-helper"]}></div>
-=======
-
-
 
   return (
     <>
     <div className={styles["course-content"]}>
     <div id="course-content" className={styles["course-content__scrollspy-helper"]}></div>
->>>>>>> 175f949696ba46da60bab6e6ddf465dc5069d477
 
                 <div className={styles["course-content__title"]}>
                     محتوي الدورة التدريبية
