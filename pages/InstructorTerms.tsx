d068eda8a38b4b4b63c789d604ebc4126470c7eb
AhmedAarafa <96011376+AhmedAarafa@users.noreply.github.com>
2022-04-12 11:45:46 +0200

    Update InstructorTerms.tsx

--- pages/InstructorTerms.tsx
+++ pages/InstructorTerms.tsx
@@ -10,7 +10,7 @@ export default function InstructorTerms() {
         <>
             <Container fluid="xxl">
                 <Navbar />
-                <InstructorTerms />
+                <InstructorTermsPage />
             </Container>
         </>
     )
