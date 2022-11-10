a37b241512b5cc5e67cc89e78e80fa4bdb9fc030
AhmedAarafa <ahmed.arafa@tadarab.com>
2022-11-10 15:43:09 +0200

    loader test

--- modules/Home page/Educational guide/EducationalGuide.tsx
+++ modules/Home page/Educational guide/EducationalGuide.tsx
@@ -3,7 +3,7 @@ import React from "react";
 import styles from "./educational-guide.module.css";
 import { Row, Col, Button } from "react-bootstrap";
 import Router from "next/router";
-import { contactUsHandler } from "modules/_Shared/utils/contactUs";
+import { contactUsHandler, openSuppotConvInNewTab } from "modules/_Shared/utils/contactUs";
 import Image from 'next/image';
 
 export default function EducationalGuide() {
@@ -14,18 +14,23 @@ export default function EducationalGuide() {
         <Col xs={12} className={styles["educational-guide"]}>
           <div className={styles["educational-guide__container"]}>
             <div className={styles["educational-guide__container__img"]}>
-                <img loading="lazy"   src={"/images/hams.png"} alt="guide" />
+              <img loading="lazy" src={"/images/TadarabForBusinessLogo.png"} alt="guide" />
             </div>
             <div className={styles["educational-guide__container__details-box"]}>
               <div className={styles["educational-guide__container__details-box__title"]}>
-                  <span> مرشدك </span>
-                  <span> التعليمي </span>
+                <span> تدرب </span>
+                <span> للشركات </span>
               </div>
               <div className={styles["educational-guide__container__details-box__breif"]}>
-              نظام دعم فني شخصي لكل متدرب لضمان نجاح كل جانب من جوانب تجربة تعلمك على تدرب لرد على استفساراتك
+                <div>
+                  تحديات سوق العمل لا تنتهي!
+                </div>
+                <div>
+                  طور مهارات مُوظفيك لتتناسب مع اقتصاد اليوم، اختر الخطة التعليمية التي تناسب أهداف عملك وتواصل معنا الآن.
+                </div>
               </div>
-              <Button onClick={()=>{ contactUsHandler() }} className={styles["educational-guide__container__details-box__btn"]}>
-              تواصل معنا
+              <Button onClick={() => { openSuppotConvInNewTab('https://wa.me/+96597128912') }} className={styles["educational-guide__container__details-box__btn"]}>
+                تواصل معنا
               </Button>
             </div>
           </div>
